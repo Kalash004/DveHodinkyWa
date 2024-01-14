4692013d0e9a783319a1be08f9be40cc191d57c7
Prince Charming <kuta.samuel@gmail.com>
2024-01-14 13:20:50 +0100

    Adding Message

--- db/sqlScripts/tempExecute.sql
+++ db/sqlScripts/tempExecute.sql
@@ -1,10 +1,10 @@
-create table IF NOT EXISTS GroupMember(
+create table IF NOT EXISTS Message(
 	id BINARY(16) primary key,
-	group_id BINARY(16),
-    constraint fk_group_id foreign key(group_id) references ChatGroup(id),
-	user_id BINARY(16),
-    constraint fk_user_id foreign key(user_id) references User(id),
-    nickname varchar(255)
+	sender_id BINARY(16) not null,
+	constraint fk_sender foreign key(sender_id) references User(id),
+	rec_id BINARY(16) not null,
+	constraint fk_rec foreign key(rec_id) references User(id),
+	message varchar(450) not null
 );
 
 
