dd24b7856b2695850ba34ce691a5b3c65feab8f4
Prince Charming <kuta.samuel@gmail.com>
2024-01-14 13:19:41 +0100

    I'm just being dumb

--- db/sqlScripts/create.sql
+++ db/sqlScripts/create.sql
@@ -22,7 +22,7 @@ create table IF NOT EXISTS GroupMember(
 	id BINARY(16) primary key,
 	group_id BINARY(16),
     constraint fk_group_id foreign key(group_id) references ChatGroup(id),
-	user_id BINARY(16) primary key,
+	user_id BINARY(16),
     constraint fk_user_id foreign key(user_id) references User(id),
     nickname varchar(255)
 );
