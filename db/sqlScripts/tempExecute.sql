7069ddfd8c83b085acfcd8bde5fc4d23ab59e17a
Prince Charming <kuta.samuel@gmail.com>
2024-01-14 13:21:39 +0100

    Adding groupMessage

--- db/sqlScripts/tempExecute.sql
+++ db/sqlScripts/tempExecute.sql
@@ -1,9 +1,9 @@
-create table IF NOT EXISTS Message(
-	id BINARY(16) primary key,
+create table IF NOT EXISTS GroupMessage(
+	id Binary(16) primary key,
 	sender_id BINARY(16) not null,
 	constraint fk_sender foreign key(sender_id) references User(id),
-	rec_id BINARY(16) not null,
-	constraint fk_rec foreign key(rec_id) references User(id),
+	group_id BINARY(16) not null,
+	constraint fk_group foreign key(group_id) references ChatGroup(id)
 	message varchar(450) not null
 );
 
