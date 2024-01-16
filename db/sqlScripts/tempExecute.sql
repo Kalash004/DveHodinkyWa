2776bcea58faff861b16c63e5faab83661c29c2d
Prince Charming <kuta.samuel@gmail.com>
2024-01-16 14:48:32 +0100

    Inserting users

--- db/sqlScripts/tempExecute.sql
+++ db/sqlScripts/tempExecute.sql
@@ -1,3 +1,9 @@
-insert into User(username,jmeno,prijmeni,email,passHash,salt) values('test0','testovan','testovich','test0@test.test','*~>?_*+>-^_!^~^ss0ese
-    ','0a06cee0f7fe25a5af0a0dbeea8cf226d4f464134cb3e1c8ee0d5a3c67a9e0f6b5f6c819475dc4b43b7b3a5b873eefcd78a4fab518a7d055f357f49504305101
-    ');
+insert into User(username,jmeno,prijmeni,email,passHash,salt) values('test1','testovanovich','tetich','test1@test.test','!>=&?>=--+!_^!$sestee
+','a1da7941cab15aca7e851d1361c4e9ac4627672295725ac719e3ba8e5bc479962dcfba24dc453734155dd41344141b262acbbe3e1fe2feacefa7d95ac39d0181
+');
+insert into User(username,jmeno,prijmeni,email,passHash,salt) values('test2','testovka','testuch','test2@test.test','_!%&@$@!@$#^~_%t2ttse
+','8eafe63b0e9eb6984133654744c4db35a459044bdd21ae81fdaa0720f48ee589266924b04c234ae3ee7388d52d114c10495f28eb38410abc0c09b0d0d7c06b0c
+');
+insert into User(username,jmeno,prijmeni,email,passHash,salt) values('test3','testovanovichka','testovova','test3@test.test','+++@-@~+>^-#+>_setste
+','ef9c86450d4a74ecc7190478e417feee95611d1a0a4b535eb55b88ef2904cc6b1644a06bf0c22d21dadd30756820e6ac9c95a88a4f1811ffd39d3700f92f9f25
+');
