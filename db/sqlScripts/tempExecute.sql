c02e7dca0f2c473e55d0ee74dd3396b951119ebd
Prince Charming <kuta.samuel@gmail.com>
2024-01-16 13:39:55 +0100

    tempExecude

--- db/sqlScripts/tempExecute.sql
+++ db/sqlScripts/tempExecute.sql
@@ -1,10 +1,3 @@
-create table IF NOT EXISTS GroupMessage(
-	id Binary(16) primary key,
-	sender_id BINARY(16) not null,
-	constraint fk_group_sender foreign key(sender_id) references User(id),
-	group_id BINARY(16) not null,
-	constraint fk_group foreign key(group_id) references ChatGroup(id),
-	message varchar(450) not null
-);
-
-
+insert into User(username,jmeno,prijmeni,email,passHash,salt) values('test0','testovan','testovich','test0@test.test','*~>?_*+>-^_!^~^ss0ese
+    ','0a06cee0f7fe25a5af0a0dbeea8cf226d4f464134cb3e1c8ee0d5a3c67a9e0f6b5f6c819475dc4b43b7b3a5b873eefcd78a4fab518a7d055f357f49504305101
+    ');
