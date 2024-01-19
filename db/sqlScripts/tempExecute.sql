2f6a73aa3053afa985c142dcda889cd38f38afc5
Prince Charming <kuta.samuel@gmail.com>
2024-01-19 22:28:35 +0100

    Yeet

--- db/sqlScripts/tempExecute.sql
+++ db/sqlScripts/tempExecute.sql
@@ -1,5 +1,5 @@
 select Message.message, User.username "sender", User.username "receiver"
 from Message inner join User sender 
 on Message.sender_id = sender.id
-from Message inner join User receiver on Message.rec_id = receiver.id
+inner join User receiver on Message.rec_id = receiver.id
 group by sender.username;
