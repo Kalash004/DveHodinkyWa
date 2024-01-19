cafa40e6477c8c63c00fec6aa1be24c2a353f66e
Prince Charming <kuta.samuel@gmail.com>
2024-01-19 22:31:13 +0100

    Random

--- db/sqlScripts/tempExecute.sql
+++ db/sqlScripts/tempExecute.sql
@@ -2,4 +2,4 @@ select Message.message, User.username "sender", User.username "receiver"
 from Message inner join User sender 
 on Message.sender_id = sender.id
 inner join User receiver on Message.rec_id = receiver.id
-group by sender.username;
+group by sender;
