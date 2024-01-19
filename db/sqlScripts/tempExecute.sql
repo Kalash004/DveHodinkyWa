5fbfd4874975c890a66ec63cc1fd3e78a6129455
Prince Charming <kuta.samuel@gmail.com>
2024-01-19 22:41:02 +0100

    Update

--- db/sqlScripts/tempExecute.sql
+++ db/sqlScripts/tempExecute.sql
@@ -1,7 +1,7 @@
-SELECT SEC_TO_TIME(
+update Message set time_sent = (SELECT SEC_TO_TIME(
           FLOOR(
              TIME_TO_SEC('01:00:00') + RAND() * (
-                  TIME_TO_SEC(TIMEDIFF('24:00:00', '00:00:00'))
+                  TIME_TO_SEC(TIMEDIFF('22:00:00', '01:00:00'))
              )
           )
-        );
+        ));
