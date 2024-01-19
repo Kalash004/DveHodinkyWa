898505b0d19013fe11381ffb404d39a80c56ccb0
Prince Charming <kuta.samuel@gmail.com>
2024-01-19 20:22:32 +0100

    Inserting group messages

--- db/sqlScripts/tempExecute.sql
+++ db/sqlScripts/tempExecute.sql
@@ -1,4 +1,4 @@
 select Message.message,User.username "sender", User.username "receiver"
 from Message inner join User
 on Message.sender_id = User.id and Message.rec_id = User.id
-group by sender;
+group by sender,message;
