9bde891ea3a5fdaa44e9c1e65b970514fea763bb
Prince Charming <kuta.samuel@gmail.com>
2024-01-19 22:44:22 +0100

    Update

--- db/sqlScripts/tempExecute.sql
+++ db/sqlScripts/tempExecute.sql
@@ -2,5 +2,5 @@ select Message.message,Message.time_sent,sender.username,receiver.username
 from Message
 inner join User sender on sender.id = Message.sender_id
 inner join User receiver on receiver.id = Message.rec_id
-group by sender.username,receiver.username
+group by sender.username,receiver.username,message
 order by time_sent;
