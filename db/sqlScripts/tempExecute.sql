d32e961c99f52393876ded99b17709200923358a
Prince Charming <kuta.samuel@gmail.com>
2024-01-19 20:13:01 +0100

    Inserting groupmembers

--- db/sqlScripts/tempExecute.sql
+++ db/sqlScripts/tempExecute.sql
@@ -1,3 +1,12 @@
-insert into ChatGroup(name,description) values("Business group","A group for conducting big business");
-insert into ChatGroup(name,description) values("Friends group","Let's chat as friends.");
-insert into ChatGroup(name,description) values("Photodump group","Dump your photos every month.");
+insert into GroupMember(group_id,user_id,nickname) values (0x4AD27EF9B6FD11EEA21006ADE036E4AC,0x2819FC7AB47611EE800F06ADE036E4AC,'Tony');
+insert into GroupMember(group_id,user_id,nickname) values (0x4AD27EF9B6FD11EEA21006ADE036E4AC,0x281AD8F5B47611EE800F06ADE036E4AC,'Pony');
+insert into GroupMember(group_id,user_id,nickname) values (0x4AD27EF9B6FD11EEA21006ADE036E4AC,0x2819FC7AB47611EE800F06ADE036E4AC,'Clony');
+
+insert into GroupMember(group_id,user_id,nickname) values (0x4AD3276DB6FD11EEA21006ADE036E4AC,0x2819FC7AB47611EE800F06ADE036E4AC,'Tony');
+insert into GroupMember(group_id,user_id,nickname) values (0x4AD3276DB6FD11EEA21006ADE036E4AC,0x281AD8F5B47611EE800F06ADE036E4AC,'Pony');
+insert into GroupMember(group_id,user_id,nickname) values (0x4AD3276DB6FD11EEA21006ADE036E4AC,0x2819FC7AB47611EE800F06ADE036E4AC,'Clony');
+
+
+insert into GroupMember(group_id,user_id,nickname) values (0x4AD3FCEDB6FD11EEA21006ADE036E4AC,0x2819FC7AB47611EE800F06ADE036E4AC,'Tony');
+insert into GroupMember(group_id,user_id,nickname) values (0x4AD3FCEDB6FD11EEA21006ADE036E4AC,0x281AD8F5B47611EE800F06ADE036E4AC,'Pony');
+insert into GroupMember(group_id,user_id,nickname) values (0x4AD3FCEDB6FD11EEA21006ADE036E4AC,0x2819FC7AB47611EE800F06ADE036E4AC,'Clony');
