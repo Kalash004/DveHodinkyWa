0640aa9e7781dc042fb8e69f98471495f36768cd
Prince Charming <kuta.samuel@gmail.com>
2024-01-20 10:03:13 +0100

    Update

--- db/sqlScripts/tempExecute.sql
+++ db/sqlScripts/tempExecute.sql
@@ -4,8 +4,8 @@ inner join User on Message.rec_id = User.id
 group by receiver,time_sent,msg
 order by receiver,time_sent\G
 
-select GroupMessage.message as msg,ChatGroup.name,GroupMessage.time_sent
+select GroupMessage.message as msg,ChatGroup.name as group,GroupMessage.time_sent as sent
 from GroupMessage
 inner join ChatGroup on GroupMessage.group_id = ChatGroup.id
-group by name,time_sent,message
-order by name,time_sent\G
+group by group,sent,message
+order by group,sent\G
