a5dcefb1f8bf1b7f7c317ae88726ecefcb5eadc3
Prince Charming <kuta.samuel@gmail.com>
2024-01-20 10:05:17 +0100

    Update

--- db/sqlScripts/tempExecute.sql
+++ db/sqlScripts/tempExecute.sql
@@ -7,5 +7,5 @@ order by receiver,time_sent\G
 select GroupMessage.message as msg,ChatGroup.name as group_Chat,GroupMessage.time_sent as sent
 from GroupMessage
 inner join ChatGroup on GroupMessage.group_id = ChatGroup.id
-group by group,sent,message
-order by group,sent\G
+group by group_Chat,sent,message
+order by group_Chat,sent\G
